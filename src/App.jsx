c50a7aceb539b55567aaa5c168c4f81dd4e1a7dd
solomias <miaveryoriginal@gmail.com>
2025-10-29 17:10:14 +0200

    styled "button" & "input" components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <div>
+          <H1>The Wild Oasis</H1>
+          <Button onClick={() => alert("Check in")}>Check in</Button>
+          <Button onClick={() => alert("Check out")}>Check out</Button>
+
+          <Input type="number" placeholder="Number of guests" />
+        </div>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
