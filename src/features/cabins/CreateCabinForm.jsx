0d1b75f15e6d4fc15bbc028504129e8251047d3a
solomias <miaveryoriginal@gmail.com>
2025-11-05 19:19:04 +0200

    images for new cabins can be added now

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -63,7 +63,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {}
@@ -143,7 +143,14 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          type="file"
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hdtmynerdmhxaugzxsou.supabase.co';
+export const supabaseUrl = 'https://hdtmynerdmhxaugzxsou.supabase.co';
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkdG15bmVyZG1oeGF1Z3p4c291Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4MzA4OTcsImV4cCI6MjA3NzQwNjg5N30.L9mF-LCM4wQ85raWNj5-_tzgmwygjYRAimw1v2q01K4";
 const supabase = createClient(supabaseUrl, supabaseKey)
 
