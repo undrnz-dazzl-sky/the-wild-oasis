77c125bf94fe0e780bcfbb9726237bc31dc569ab
solomias <miaveryoriginal@gmail.com>
2025-11-15 18:38:40 +0200

    user now can update their data, avatar & change password

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -79,7 +79,6 @@ function SignupForm() {
       </FormRow>
 
       <FormRow>
-        {/* type is an HTML attribute! */}
         <Button
           variation="secondary"
           type="reset"
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase.js';
+import supabase, { supabaseUrl } from './supabase.js';
 
 export async function signup({ fullName, email, password }) {
     const { data, error } = await supabase.auth.signUp({
@@ -47,3 +47,50 @@ export async function logout() {
     const { error } = await supabase.auth.signOut();
     if (error) throw new Error(error.message);
 };
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+
+    let updateData;
+    if (password) updateData = { password };
+    if (fullName) updateData = { data: { fullName } };
+
+    const { data, error: dataUpdateError } = await supabase.auth.updateUser(
+        updateData
+    );
+
+    if (dataUpdateError) throw new Error(dataUpdateError.message);
+
+    console.log(data.user.user_metadata.avatar?.split("/").at(-1));
+    if (!avatar) return data;
+
+    const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+    const hasImage = data.user.user_metadata.avatar;
+
+    if (hasImage) {
+        const existingFilePath = data.user.user_metadata.avatar.split("/")?.at(-1);
+
+        const { data: imageDeleteData, error: imageDeleteError } =
+            await supabase.storage.from("avatars").remove([existingFilePath]);
+
+        if (imageDeleteError) throw new Error(imageDeleteError.message);
+        console.log("Deleted image ", imageDeleteData);
+    }
+
+    const { error: imageUploadError } = await supabase.storage
+        .from("avatars")
+        .upload(fileName, avatar);
+
+    if (imageUploadError) throw new Error(imageUploadError.message);
+
+    const { data: avatarUpdatedData, error: avatarUpdateError } =
+        await supabase.auth.updateUser({
+            data: {
+                avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+            },
+        });
+
+    if (avatarUpdateError) throw new Error(avatarUpdateError.message);
+
+    return avatarUpdatedData;
+}
